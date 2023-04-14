92aab8368e29f072206b697b1bfad0b198d7334e
wxxni <91862274+woOONi@users.noreply.github.com>
2023-04-14 09:26:16 +0900

    [level 0] Title: 모음 제거, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
@@ -1,2 +1,5 @@
+import re
 def solution(my_string):
-    return my_string.replace('a', '').replace('e', '').replace('i', '').replace('o', '').replace('u', '')
+    return re.sub(r'[aeiou]','',my_string)
+
+# sub: 제거하거나 교체할때 사용
