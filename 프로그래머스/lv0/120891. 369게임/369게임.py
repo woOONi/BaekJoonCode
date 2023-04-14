87318d4ccb5be7362fe9cc599ecf175be3db2e99
wxxni <91862274+woOONi@users.noreply.github.com>
2023-04-14 11:09:42 +0900

    [level 0] Title: 369게임, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.py
+++ 프로그래머스/lv0/120891. 369게임/369게임.py
@@ -4,5 +4,9 @@
 #     answer += str(order).count('9')
 #     return answer
 
+# def solution(order):
+#     return sum(map(lambda x : str(order).count(x), '369'))
+
+import re
 def solution(order):
-    return sum(map(lambda x : str(order).count(x), '369'))
+    return len(re.sub('[^369]','',str(order)))
