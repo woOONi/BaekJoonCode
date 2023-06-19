aed81dd4acbeafeacc02316e834d04940d4c086b
wxxni <91862274+woOONi@users.noreply.github.com>
2023-06-19 10:02:01 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,14 +1,26 @@
--- 코드를 입력하세요
-WITH TOP_MEM AS(
-    SELECT MEMBER_ID
-    FROM REST_REVIEW
-    GROUP BY MEMBER_ID
-    ORDER BY COUNT(REVIEW_ID) DESC
-    LIMIT 1
-)
+# 몰름개오래걸림
+# 서브쿼리랑 LIMIT 같이 사용안된다해서 눈물남
+# SELECT M.MEMBER_NAME, R.REVIEW_TEXT, DATE_FORMAT(R.REVIEW_DATE, "%Y-%m-%d")
+# FROM MEMBER_PROFILE M
+# JOIN (SELECT REVIEW_TEXT, REVIEW_DATE, MEMBER_ID
+#       FROM REST_REVIEW
+#       WHERE MEMBER_ID IN (SELECT MEMBER_ID
+#                          FROM REST_REVIEW
+#                          GROUP BY MEMBER_ID
+#                          ORDER BY COUNT(*) DESC
+#                          LIMIT 2)
+#      ) R
+# ON R.MEMBER_ID = M.MEMBER_ID
+# ORDER BY R.REVIEW_DATE, R.REVIEW_TEXT
 
-SELECT MEMBER_NAME, REVIEW_TEXT, DATE_FORMAT(REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
-FROM REST_REVIEW AS R
-LEFT JOIN MEMBER_PROFILE AS M ON M.MEMBER_ID = R.MEMBER_ID
-JOIN TOP_MEM AS T ON T.MEMBER_ID = R.MEMBER_ID
-ORDER BY REVIEW_DATE, REVIEW_TEXT
+select p.MEMBER_NAME, r.REVIEW_TEXT, date_format(r.REVIEW_DATE, "%Y-%m-%d")
+from (
+    select MEMBER_ID, rank() over (order by count(REVIEW_ID) desc) as rnk
+    from REST_REVIEW
+    group by MEMBER_ID
+    # having rnk = 1
+) tmp1
+left join REST_REVIEW r on tmp1.MEMBER_ID = r.MEMBER_ID
+left join MEMBER_PROFILE p on tmp1.MEMBER_ID = p.MEMBER_ID
+where rnk = 1
+order by REVIEW_DATE, REVIEW_TEXT
