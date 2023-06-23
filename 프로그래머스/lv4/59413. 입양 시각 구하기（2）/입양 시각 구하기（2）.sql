08bd31e498df28846d9fe5a1ebc9b93c7d98e4d7
wxxni <91862274+woOONi@users.noreply.github.com>
2023-06-23 09:37:34 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,16 +1,32 @@
--- 코드를 입력하세요
-WITH RECURSIVE H AS (
-    SELECT 0 AS N
+# -- 코드를 입력하세요
+# WITH RECURSIVE H AS (
+#     SELECT 0 AS N
+#     UNION ALL
+#     SELECT N+1 H FROM H WHERE N < 23
+# ),
+# C AS (
+#     SELECT HOUR(DATETIME) AS HOUR, COUNT(ANIMAL_ID) AS COUNT
+#     FROM ANIMAL_OUTS
+#     GROUP BY HOUR
+# )
+
+# SELECT H.N AS HOUR, IFNULL(C.COUNT, 0) AS COUNT
+# FROM H 
+# LEFT JOIN C
+# ON H.N = C.HOUR
+
+WITH RECURSIVE number 
+AS ( 
+    SELECT 0 AS n
     UNION ALL
-    SELECT N+1 H FROM H WHERE N < 23
-),
-C AS (
-    SELECT HOUR(DATETIME) AS HOUR, COUNT(ANIMAL_ID) AS COUNT
-    FROM ANIMAL_OUTS
-    GROUP BY HOUR
+    SELECT n + 1 AS n 
+    FROM number
+    WHERE n < 23
 )
 
-SELECT H.N AS HOUR, IFNULL(C.COUNT, 0) AS COUNT
-FROM H 
-LEFT JOIN C
-ON H.N = C.HOUR
+select n as HOUR, count(ANIMAL_ID) as COUNT
+from number n
+left join ANIMAL_OUTS a
+on hour(a.DATETIME) = n.n
+group by HOUR
+order by HOUR
