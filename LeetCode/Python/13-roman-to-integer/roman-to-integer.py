bab145714ef1c535ddaa9fe2c0242b0861b723e6
wxxni <91862274+woOONi@users.noreply.github.com>
2024-03-06 09:51:40 +0900

    Time: 50 ms (38.47%) | Memory: 16.5 MB (77.46%) - LeetSync

---
+++ LeetCode/Python/13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        tmp = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000
+        }
+        ans = 0
+
+        for i in range(len(s)):
+            if i < len(s)-1 and tmp[s[i]] < tmp[s[i+1]]:
+                ans -= tmp[s[i]]
+            else:
+                ans += tmp[s[i]]
+        
+        return ans
