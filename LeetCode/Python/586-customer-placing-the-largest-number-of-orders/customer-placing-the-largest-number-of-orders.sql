93a196b3f8f0a21b834a681cf0cb2bb94c1f3963
wxxni <91862274+woOONi@users.noreply.github.com>
2024-03-07 19:41:22 +0900

    Time: 1004 ms (33.95%) | Memory: 0B (100.00%) - LeetSync

---
+++ LeetCode/Python/586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select customer_number
+from(
+    select customer_number, count(order_number) as cnt
+    from Orders
+    group by customer_number
+    order by cnt desc
+)a
+limit 1
