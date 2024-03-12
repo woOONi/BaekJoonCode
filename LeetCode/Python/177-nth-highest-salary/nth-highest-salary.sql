82746842411ef54a506c6c94c53a8c1cc3da1af2
wxxni <91862274+woOONi@users.noreply.github.com>
2024-03-12 12:54:56 +0900

    Time: 1127 ms (16.41%) | Memory: 0B (100.00%) - LeetSync

---
+++ LeetCode/Python/177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE m INT; 
+set m = n-1;
+  RETURN (
+      # Write your MySQL query statement below.
+        select distinct salary
+        from Employee
+        order by salary desc
+        limit 1
+        offset m
+  );
+END
