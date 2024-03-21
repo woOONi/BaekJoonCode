ca9d810afe26d98b7cb5f56cd51f7c5baa711c01
wxxni <91862274+woOONi@users.noreply.github.com>
2024-03-21 18:39:43 +0900

    Time: 128 ms (10.87%) | Memory: 18.5 MB (73.16%) - LeetSync

--- LeetCode/Python/42-trapping-rain-water/trapping-rain-water.py
+++ LeetCode/Python/42-trapping-rain-water/trapping-rain-water.py
@@ -1,49 +1,24 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # two pointer 풀이
-        if not height:
-            return 0
-        
+        # stack
+        stack = []
         volume = 0
-        left, right = 0, len(height)-1
-        left_max, right_max = height[left], height[right]
-
-        while left < right:
-            left_max, right_max = max(left_max, height[left]),max(right_max, height[right])
-            # 더 높은 쪽을 향하여 투 포인터 이동
-            if left_max <= right_max:
-                volume += left_max - height[left]
-                # print('left:',left,left_max)
-                # print('volume:',volume)
-                left += 1
+        
+        for i in range(len(height)):
+            # 변곡점을 만나는 경우
+            while stack and height[i] > height[stack[-1]]:
+                # 스택에서 빠진다
+                top = stack.pop()
+                print(i,' top:', top)
+                if not len(stack):
+                    break
                 
-            else:
-                volume += right_max - height[right]
-                # print('right:',right,right_max)
-                # print('volume:',volume)
-                right -=1
-            
+                # 이전과의 차이만큼 물높이 처리
+                distance = i - stack[-1] - 1
+                waters = min(height[i], height[stack[-1]]) - height[top]
+                volume += distance * waters
+                print(distance, waters, volume)
+
+            stack.append(i)
         return volume
 
-# left: 0 0
-# volume: 0
-# left: 1 1
-# volume: 0
-# left: 2 1
-# volume: 1
-# right: 11 1
-# volume: 1
-# left: 3 2
-# volume: 1
-# left: 4 2
-# volume: 2
-# left: 5 2
-# volume: 4
-# left: 6 2
-# volume: 5
-# right: 10 2
-# volume: 5
-# right: 9 2
-# volume: 6
-# right: 8 2
-# volume: 6
