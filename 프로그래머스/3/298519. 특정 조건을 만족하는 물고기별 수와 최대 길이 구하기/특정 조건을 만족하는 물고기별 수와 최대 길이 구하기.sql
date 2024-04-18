80f1f42c7b4279499b745a93e43e558371393b09
wxxni <91862274+woOONi@users.noreply.github.com>
2024-04-18 16:00:30 +0900

    [level 3] Title: 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -1,17 +1,34 @@
--- 코드를 작성해주세요
-WITH avg_fish as(
+# -- 코드를 작성해주세요
+# WITH avg_fish as(
+#     SELECT FISH_TYPE
+#     , AVG(COALESCE(LENGTH,10)) avg_length
+#     FROM FISH_INFO
+#     GROUP BY FISH_TYPE
+#     HAVING avg_length >= 33
+# )
+
+# SELECT 
+#     COUNT(distinct ID) FISH_COUNT
+#     , MAX(LENGTH) MAX_LENGTH
+#     , i.FISH_TYPE
+# FROM avg_fish a
+# LEFT JOIN FISH_INFO i ON i.FISH_TYPE = a.FISH_TYPE
+# GROUP BY FISH_TYPE
+# ORDER BY FISH_TYPE
+
+
+WITH avg_fish as (
     SELECT FISH_TYPE
-    , AVG(COALESCE(LENGTH,10)) avg_length
+        , avg(coalesce(length, 10)) as avg_length
     FROM FISH_INFO
     GROUP BY FISH_TYPE
     HAVING avg_length >= 33
 )
 
-SELECT 
-    COUNT(distinct ID) FISH_COUNT
-    , MAX(LENGTH) MAX_LENGTH
+SELECT COUNT(ID) as FISH_COUNT
+    , MAX(length) as MAX_LENGTH
     , i.FISH_TYPE
-FROM avg_fish a
-LEFT JOIN FISH_INFO i ON i.FISH_TYPE = a.FISH_TYPE
+FROM FISH_INFO i
+JOIN avg_fish a ON i.FISH_TYPE = a.FISH_TYPE
 GROUP BY FISH_TYPE
 ORDER BY FISH_TYPE
