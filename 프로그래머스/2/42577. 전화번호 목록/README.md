3256474ea4ed0953fad99065f306cf944b29be43
wxxni <91862274+woOONi@users.noreply.github.com>
2025-03-19 14:24:10 +0900

    [level 2] Title: 전화번호 목록, Time: 448.60 ms, Memory: 45.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3) 
 
 ### 성능 요약
 
-메모리: 28 MB, 시간: 80.63 ms
+메모리: 45.7 MB, 시간: 448.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 20일 16:42:17
+2025년 03월 19일 14:24:07
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,28 @@
+# def solution(phone_book):
+#     answer = True
+#     hash_map = {}
+#     for p in phone_book:
+#         hash_map[p] = 1
+#     for phone_number in phone_book:
+#         temp = ""
+#         for number in phone_number:
+#             temp += number
+#             if temp in hash_map and temp != phone_number:
+#                 answer = False
+#     return answer
+
+
+
+
+
 def solution(phone_book):
-    phone_book.sort()
-    for p1, p2 in zip(phone_book, phone_book[1:]):
-        if p2.startswith(p1):
-            return False
+    dic = {}
+    for phone_number in phone_book:
+        dic[phone_number] = 1
+    for phone_number in phone_book:
+        tmp = ""
+        for number in phone_number:
+            tmp += number
+            if tmp in dic and tmp != phone_number:
+                return False
     return True
