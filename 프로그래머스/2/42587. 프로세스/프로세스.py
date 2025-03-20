4380c1a795695299565e3fbfb7f6055ef99e66ca
wxxni <91862274+woOONi@users.noreply.github.com>
2025-03-20 12:33:48 +0900

    [level 2] Title: 프로세스, Time: 0.23 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,13 +1,13 @@
 from collections import deque
 def solution(priorities, location):
-    prior = [[i, x] for i, x in enumerate(priorities)]
-    cnt = 0
-    q = deque(prior)
-    while q:
-        now = q.popleft()
-        if any(now[1] < queue[1] for queue in q):
-            q.append(now)
-        else:
-            if now[0] == location:
-                return cnt+1
-            cnt += 1
+    l = len(priorities)
+    queue = deque([(i,p) for i,p in enumerate(priorities)])
+    
+    max_value = max(priorities)
+    while queue:
+        idx, p = queue.popleft()
+        if p == max_value and idx == location: break
+        elif p == max_value: priorities.remove(p); max_value = max(priorities)
+        else : queue.append((idx,p))
+        
+    return l - len(queue)
